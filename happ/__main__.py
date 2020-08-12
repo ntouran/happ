16e3df079cf7b7496e672e01d106c790b6d225a8
Nick Touran <ntouran@terrapower.com>
2020-08-12 16:29:32 -0700

    Do register app when running as main

--- happ/__main__.py
+++ happ/__main__.py
@@ -1,5 +1,12 @@
 import sys
 from armi.cli import ArmiCLI
+import armi
+
+from happ import app
+
+# if running this as an app, we must register the app
+# (otherwise we may be running as a plugin and don't want to register it)
+armi.configure(app.HallamApp())
 
 
 def main():
